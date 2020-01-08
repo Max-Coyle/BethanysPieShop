363b52e479c81b5c0b9c360f8c1991fefe6dd575
Max Coyle <55137324@ad.mmu.ac.uk>
2020-01-08 11:49:52 +0000

    Initial Commit

---
+++ BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            var piesListViewModel = new PiesListViewModel
+            {
+                Pies = _pieRepository.AllPies, 
+                CurrentCategory = "Cheese cakes"
+            };
+            return View(piesListViewModel);
+        }
+    }
+}
